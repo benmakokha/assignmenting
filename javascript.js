92cf9f704431a18545bd6df3e9e4b6431e30997f
benmakokha <80826990+benmakokha@users.noreply.github.com>
2021-07-03 19:32:43 +0300

    Bens' first project

--- javascript.js
+++
@@ -1,5 +0,0 @@
-alert("Confirm your name and gender to proceed!");
-document.write("more scripts to be coded <br>");
-document.write("This is pure java script<br>");
-var name=3;
-document.write("my number is"+" "+name);
